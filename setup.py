b074b597e589e148122d0712675187b98bfc20e3
Tony Cebzanov <tonycpsu@gmail.com>
2018-03-05 12:03:08 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from glob import glob
 
 name = 'panwid'
 setup(name=name,
-      version='0.2.2.dev3',
+      version='0.2.2.dev4',
       description='Useful widgets for urwid',
       author='Tony Cebzanov',
       author_email='tonycpsu@gmail.com',
