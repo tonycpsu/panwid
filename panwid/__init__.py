9a2bf81356282315445ecf9fe7253894032f132d
Tony Cebzanov <tonycpsu@gmail.com>
2019-03-31 13:50:14 -0400

    Bump version: 0.3.0.dev8 → 0.3.0.dev9

--- panwid/__init__.py
+++ panwid/__init__.py
@@ -13,7 +13,7 @@ from .scroll import *
 from . import tabview
 from .tabview import *
 
-__version__ = "0.3.0.dev8"
+__version__ = "0.3.0.dev9"
 
 __all__ = (
     listbox.__all__
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from glob import glob
 
 name = 'panwid'
 setup(name=name,
-      version='0.3.0.dev8',
+      version='0.3.0.dev9',
       description='Useful widgets for urwid',
       author='Tony Cebzanov',
       author_email='tonycpsu@gmail.com',
